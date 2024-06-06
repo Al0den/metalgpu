116241cc5ff6ca571716f1bfe9833c73c9f919c2
Al0den <111601320+Al0den@users.noreply.github.com>
2024-06-06 16:27:14 +0200

    Fix: examples

--- examples/adder.py
+++ examples/adder.py
@@ -2,7 +2,7 @@ import metalgpu
 import ctypes
 import numpy as np
 
-instance = metalgpu.Interface() # Initialise the metal instance
+instance = metalgpu.Interface()  # Initialise the metal instance
 shader_string = """
 #include <metal_stdlib>
 
@@ -13,12 +13,12 @@ kernel void adder(device int *arr1 [[buffer(0)]], device int *arr2 [[buffer(1)]]
 }
 """
 instance.load_shader_from_string(shader_string)
-instance.set_function("adder") # Name of the function that will be ran (Can be changed at any time)
+instance.set_function("adder")
 
-buffer_size = 100000 # Number of items in the buffer
-buffer_type = ctypes.c_int # Types of the items inside the buffer
+buffer_size = 100000  # Number of items in the buffer
+buffer_type = ctypes.c_int  # Types of the items inside the buffer
 
-buffer1 = instance.create_buffer(buffer_size, buffer_type) # Create a shared gpu-cpu buffer. Can be accessed as a numpy array from buffer1.contents
+buffer1 = instance.create_buffer(buffer_size, buffer_type)
 buffer2 = instance.create_buffer(buffer_size, buffer_type)
 buffer3 = instance.create_buffer(buffer_size, buffer_type)
 
@@ -26,10 +26,10 @@ for i in range(buffer_size):
     buffer1.contents[i] = i
     buffer2.contents[i] = int(np.sqrt(i))
 
-instance.run_function(buffer_size, [buffer1, buffer2, buffer3]) # Computes i + sqrt(i)
+instance.run_function(buffer_size, [buffer1, buffer2, buffer3])
 
 for i in range(buffer_size):
-    assert(buffer3.contents[i] == i + int(np.sqrt(i))) 
+    assert(buffer3.contents[i] == i + int(np.sqrt(i)))
 
 buffer1.release()
 buffer2.release()
