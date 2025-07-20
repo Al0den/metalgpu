2e4433cbdefef0cf2c2b82a1dee4b1653b58268d
James Bryan Graves <jbg@Macintosh-2.local>
2025-07-20 11:00:56 +0100

    instance counting, thread safe

--- examples/adder.py
+++ examples/adder.py
@@ -1,35 +1,38 @@
 import metalgpu
 
-instance = metalgpu.Interface()  # Initialise the metal instance
-shader_string = """
-#include <metal_stdlib>
 
-using namespace metal;
+for i in range(3):
+    instance = metalgpu.Interface()  # Initialise the metal instance inside loop - this will cause segfault
 
-kernel void adder(device int *arr1 [[buffer(0)]],
-        device int *arr2 [[buffer(1)]],
-        device int *arr3 [[buffer(2)]],
-        uint id [[thread_position_in_grid]]) {
-    arr3[id] = arr2[id] + arr1[id];
-}
-"""
-instance.load_shader_from_string(shader_string)
-instance.set_function("adder")
+    shader_string = """
+    #include <metal_stdlib>
 
-buffer_size = 100000  # Number of items in the buffer
-buffer_type = "int"
+    using namespace metal;
 
-buffer1 = instance.create_buffer(buffer_size, buffer_type)
-buffer2 = instance.create_buffer(buffer_size, buffer_type)
-buffer3 = instance.create_buffer(buffer_size, buffer_type)
+    kernel void adder(device int *arr1 [[buffer(0)]],
+            device int *arr2 [[buffer(1)]],
+            device int *arr3 [[buffer(2)]],
+            uint id [[thread_position_in_grid]]) {
+        arr3[id] = arr2[id] + arr1[id];
+    }
+    """
+    instance.load_shader_from_string(shader_string)
+    instance.set_function("adder")
 
-buffer1.contents[:] = [i for i in range(buffer_size)]
-buffer2.contents[:] = [i for i in range(buffer_size)]
+    buffer_size = 100000  # Number of items in the buffer
+    buffer_type = "int"
 
-instance.run_function(buffer_size, [buffer1, buffer2, buffer3])
+    buffer1 = instance.create_buffer(buffer_size, buffer_type)
+    buffer2 = instance.create_buffer(buffer_size, buffer_type)
+    buffer3 = instance.create_buffer(buffer_size, buffer_type)
 
-assert(all(buffer3.contents == [i * 2 for i in range(buffer_size)]))
+    buffer1.contents[:] = [i for i in range(buffer_size)]
+    buffer2.contents[:] = [i for i in range(buffer_size)]
 
-buffer1.release()  # This isnt required, pythons should automatically do it
-buffer2.release()
-buffer3.release()
+    instance.run_function(buffer_size, [buffer1, buffer2, buffer3])
+
+    assert(all(buffer3.contents == [i * 2 for i in range(buffer_size)]))
+
+    buffer1.release()  # This isnt required, pythons should automatically do it
+    buffer2.release()
+    buffer3.release()
--- metal-gpu-c/src/main.cpp
+++ metal-gpu-c/src/main.cpp
@@ -4,56 +4,82 @@
 
 #include "config.h"
 #include "instance/instance.h"
+#include <mutex>
 
-Instance *mainInstance;
+Instance *mainInstance = nullptr;
+int instanceRefCount = 0;
+std::mutex instanceMutex;  // Thread safety for singleton management
+
+// Helper function to ensure instance exists
+static bool ensureInstance() {
+    return mainInstance != nullptr;
+}
 
 int main() {}  
 
 extern "C" {
     void init() {
-        mainInstance = new Instance();
-        mainInstance->init();
+        std::lock_guard<std::mutex> lock(instanceMutex);
+        if (mainInstance == nullptr) {
+            mainInstance = new Instance();
+            mainInstance->init();
+        }
+        instanceRefCount++;
     }
 
     void createLibrary(const char *filename) {
+        if (!ensureInstance()) return;
         mainInstance->createLibrary(filename);
     }
 
     int createBuffer(int bufsize) {
+        if (!ensureInstance()) return -1;
         int bufferNumber = mainInstance->createBuffer(bufsize);
         return bufferNumber;
     }
 
     void setFunction(const char *funcname) {
+        if (!ensureInstance()) return;
         mainInstance->setFunction(funcname);
     }
 
     void runFunction(int *MetalSize, int *requestedBuffers, int numRequestedBuffers, bool waitForCompletion) {
+        if (!ensureInstance()) return;
         mainInstance->runFunction(MetalSize, requestedBuffers, numRequestedBuffers, waitForCompletion);
     }
 
     void releaseBuffer(int bufnum) {
+        if (!ensureInstance()) return;
         mainInstance->releaseBuffer(bufnum);
     }
 
     void deleteInstance() {
-        delete mainInstance;
-        mainInstance = nullptr;
+        std::lock_guard<std::mutex> lock(instanceMutex);
+        instanceRefCount--;
+        if (instanceRefCount <= 0 && mainInstance != nullptr) {
+            delete mainInstance;
+            mainInstance = nullptr;
+            instanceRefCount = 0;  // Safety reset
+        }
     }
 
     void *getBufferPointer(int bufnum) {
+        if (!ensureInstance()) return nullptr;
         return mainInstance->getBufferPointer(bufnum);
     }
 
     void createLibraryFromString(const char* string) {
+        if (!ensureInstance()) return;
         mainInstance->createLibraryFromString(string);
     }
 
     int maxThreadsPerGroup() {
+        if (!ensureInstance()) return 0;
         return mainInstance->maxThreadsPerGroup();
     }
 
     int threadExecutionWidth() {
+        if (!ensureInstance()) return 0;
         return mainInstance->threadExecutionWidth();
     }
 }
